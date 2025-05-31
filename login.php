c29ec5e4eaa6538799a999d0fc2bfc492191cb8a
Jeevanstar2 <jeevanthomasjaison01@algomau.ca>
2025-05-31 00:17:12 +0000

    now added a link of "Create a new account?" option into the login page to access the create a new account page

--- login.php
+++ login.php
@@ -25,12 +25,15 @@ if (!isset($_SESSION['login_attempts']))
 <body>
 <h2>Login Page</h2>
 <form action="validate.php" method="POST">
-    Username: <input type="text" name="username"><br><br>
-    Password: <input type="password" name="password"><br><br>
+    Username: <input type="text" name="username" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
     <?php if (isset($_GET['error'])): ?>
         <p class="error-msg"><?php echo htmlspecialchars($_GET['error']); ?></p>
     <?php endif; ?>
     <input type="submit" value="Login">
 </form>
+
+<p>Don't have an account? <a href="register.php">Create a new account</a></p>
+
 </body>
 </html>
